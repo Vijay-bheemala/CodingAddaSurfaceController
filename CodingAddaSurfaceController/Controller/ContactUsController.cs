d3b270ba09073d1854ce65d6b2fb0a02ec01c611
VijayBheemala <VijayBheemala@WSIN152>
2024-11-04 09:49:39 +0530

    Add project files.

---
+++ CodingAddaSurfaceController/Controller/ContactUsController.cs
@@ -0,0 +1,76 @@
+﻿using CodingAddaSurfaceController.Models;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Crypto.Macs;
+using System.Net;
+using System.Net.Mail;
+using System.Net.NetworkInformation;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace CodingAddaSurfaceController.Controller
+{
+	public class ContactUsController : SurfaceController
+	{
+		public ContactUsController(
+			IUmbracoContextAccessor umbracoContextAccessor,
+			IUmbracoDatabaseFactory databaseFactory,
+			ServiceContext services,
+			AppCaches appCaches,
+			IProfilingLogger profilingLogger,
+			IPublishedUrlProvider publishedUrlProvider)
+			: base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+		{
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult HandleSubmit(ContactUs contactModel)
+		{
+			if (!ModelState.IsValid)
+			{
+				// Return to the current page with validation errors
+				return CurrentUmbracoPage();
+			}
+
+			// Send email
+			try
+			{
+				SendEmail(contactModel);
+				TempData["Success"] = "Page submitted successfully with " + contactModel.Name;
+			}
+			catch (Exception ex)
+			{
+				TempData["Error"] = "There was an error sending your message: " + ex.Message;
+			}
+
+			return RedirectToCurrentUmbracoPage();
+		}
+
+		private void SendEmail(ContactUs model)
+		{
+			var smtpClient = new SmtpClient("smtp.gmail.com")
+			{
+				Port = 587,
+				Credentials = new NetworkCredential("bheemala.vijay@gmail.com", "pufh pbtw ogzd vmnx"), // Use App Password if 2FA is enabled
+				EnableSsl = true,
+			};
+
+			var mailMessage = new MailMessage
+			{
+				From = new MailAddress(model.Email),
+				Subject = "New Contact Form Submission",
+				Body = $"Name: {model.Name}\nEmail: {model.Email}\nMessage: {model.Message}",
+				IsBodyHtml = false,
+			};
+
+			mailMessage.To.Add("vijay.bheemala@websynergies.com"); // Change to your recipient email
+
+			smtpClient.Send(mailMessage);
+		}
+	}
+}
---
+++ CodingAddaSurfaceController/Models/ContactUs.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CodingAddaSurfaceController.Models
+{
+	public class ContactUs
+	{
+		[Required]
+		public string Name { get; set; }	
+		[Required]
+		public string Email { get; set; }
+		[Required]
+		public string Message { get; set; }
+
+	}
+}
